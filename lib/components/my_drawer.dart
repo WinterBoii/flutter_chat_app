b5734b11f7b55040e953ee71148e17e92cdda8c5
WinterBoii <wintercodess@gmail.com>
2024-07-22 22:16:56 +0200

    create drawer

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import '../auth/auth_service.dart';
+import '../pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  void logout() async {
+    // Implement logout logic here
+    final auth = AuthService();
+
+    await auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40,
+                  ),
+                ),
+              ),
+              // home tile
+              ListTile(
+                title: const Text('H O M E'),
+                leading: Icon(
+                  Icons.home,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                onTap: () {
+                  // just pop
+                  Navigator.pop(context);
+                },
+              ),
+
+              // settings tile
+              ListTile(
+                title: const Text('S E T T I N G S'),
+                leading: Icon(
+                  Icons.settings,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          // logout tile
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25.0),
+            child: ListTile(
+              title: const Text('L O G O U T'),
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
