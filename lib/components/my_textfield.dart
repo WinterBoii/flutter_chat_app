f19963d1704f63005973dc8a13da71ea681fc3bb
WinterBoii <wintercodess@gmail.com>
2024-07-22 22:16:47 +0200

    update textfield

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -1,44 +1,75 @@
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
-class MyTextField extends StatelessWidget {
+class MyTextField extends StatefulWidget {
   final String hintText;
   final TextEditingController controller;
 
   bool hide = false;
 
-  MyTextField(
-      {super.key,
-      required this.hintText,
-      this.hide = false,
-      required this.controller});
+  MyTextField({super.key, required this.hintText, this.hide = false, required this.controller});
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  void clearField() {
+    setState(() {
+      widget.controller.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.tertiary,
-            ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Theme.of(context).colorScheme.secondary,
+          border: Border.all(
+            color: Theme.of(context).colorScheme.tertiary,
           ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ),
-          filled: true,
-          fillColor: Theme.of(context).colorScheme.secondary,
-          hintStyle: TextStyle(
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          hintText: hintText,
         ),
-        obscureText: hide,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: widget.controller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                  hintStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  hintText: widget.hintText,
+                ),
+                obscureText: widget.hide,
+              ),
+            ),
+            IconButton(
+              onPressed: () => clearField(),
+              icon: Icon(
+                Icons.clear,
+                size: 18,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
